7985bfdc9c36a1526c56d20fbb74c2b8f77405d0
laonvie <161924787+laonvie@users.noreply.github.com>
2024-03-03 12:37:32 +0900

    Kocom.py modified

--- kocom.py
+++ kocom.py
@@ -38,7 +38,8 @@ chksum_position = 18  # 18th byte
 type_t_dic = {'30b':'send', '30d':'ack'}
 seq_t_dic = {'c':1, 'd':2, 'e':3, 'f':4}
 # device_t_dic = {'01':'wallpad', '0e':'light', '2c':'gas', '36':'thermo', '3b': 'plug', '44':'elevator', '48':'fan'}  # 2023.08 AC, AIR 추가
-device_t_dic = {'01': 'wallpad', '0e': 'light', '2c': 'gas', '36': 'thermo', '39': 'ac', '3b': 'plug', '44': 'elevator', '48': 'fan', '98': 'air'}
+# device_t_dic = {'01': 'wallpad', '0e': 'light', '2c': 'gas', '36': 'thermo', '39': 'ac', '3b': 'plug', '44': 'elevator', '48': 'fan', '98': 'air'}
+device_t_dic = {'01': 'wallpad', '0e': 'light', '2c': 'gas', '36': 'thermo', '44': 'elevator', '48': 'fan'}
 cmd_t_dic = {'00':'state', '01':'on', '02':'off', '3a':'query'}
 room_t_dic = {'00':'livingroom', '01':'room1', '02':'room2', '03':'room3', '04':'kitchen'}
 
@@ -309,7 +310,7 @@ def fan_parse(value):
     if logtxt != "" and config.get('Log', 'show_recv_hex') == 'True':
         logging.info(logtxt)
     return { 'state': state, 'preset': preset}
-'''
+
 # 2023.08 AC 추가
 def ac_parse(value):
     mode_dic = {'00': 'cool', '01': 'fan_only', '02': 'dry', '03': 'auto'}
@@ -323,7 +324,7 @@ def ac_parse(value):
     if logtxt != '' and config.get('Log', 'show_recv_hex') == 'True':
         logging.info(logtxt)
     return {'state': state, 'fan': fan, 'temperature': temperature, 'target': target}
-'''
+
 # query device --------------------------
 
 def query(device_h, publish=False, enforce=False):
@@ -443,7 +444,6 @@ def mqtt_on_message(mqttc, obj, msg):
         value = '1100' + settemp_hex + '0000000000'
         send_wait_response(dest=dev_id, value=value, log='thermo settemp')
 
-    '''
     # 2023.08 AC 추가
     elif 'ac' in topic_d and 'ac_mode' in topic_d:
         is_on = '10' if command != 'off' else '00'
@@ -472,7 +472,6 @@ def mqtt_on_message(mqttc, obj, msg):
         value = '1010000000' + settemp_hex + '0000'
         send_wait_response(dest=dev_id, value=value, log='ac settemp')
  
-    '''    
     # light on/off : kocom/livingroom/light/1/command
     elif 'light' in topic_d:
         dev_id = device_h_dic['light'] + room_h_dic.get(topic_d[1])
@@ -568,14 +567,10 @@ def packet_processor(p):
             state = thermo_parse(p['value'])
             logtxt='[MQTT publish|thermo] id[{}] data[{}]'.format(p['src_subid'], state)
             mqttc.publish("kocom/room/thermo/" + p['src_subid'] + "/state", json.dumps(state))
-            
-        """
         elif p['src'] == 'ac' and p['cmd'] == 'state':
             state = ac_parse(p['value'])
             logtxt = '[MQTT publish|ac] id[{}] data[{}]'.format(p['src_subid'], state)
             mqttc.publish('kocom/room/ac/' + p['src_subid'] + '/state', json.dumps(state), retain=True)
-        """
-
         elif p['src'] == 'air':
             if int(p['value'], 16) > 0:
                 state = air_parse(p['value'])
